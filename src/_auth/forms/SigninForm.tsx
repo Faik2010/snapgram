2e670903bc69ccd514c1b0729aa755dca7538e02
Faik <faikpirincci2010@gmail.com>
2023-11-04 00:16:59 +0300

    sidebar and bottom bar

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -12,11 +12,11 @@ import {
 import { Input } from "@/components/ui/input"
 
 import { Button } from "@/components/ui/button"
-import { SignupValidation } from "@/lib/validation"
+import { SigninValidation as SigninValidation } from "@/lib/validation"
 import Loader from "@/components/shared/Loader"
 import { Link, useNavigate } from "react-router-dom"
 import { useToast } from "@/components/ui/use-toast"
-import { useCreateUserAccount, useSignInAccount } from "@/lib/react-query/queriesAndMutations"
+import { useSignInAccount } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
 
 
@@ -27,27 +27,18 @@ const SigninForm = () => {
  const {checkAuthUser, isLoading:isUserLoading} = useUserContext()
  const navigate = useNavigate()
 
-  const {mutateAsync: createUserAccount, isPending: isCreatingUser} = useCreateUserAccount();
 
-  const {mutateAsync: signInAccount, isPending:isSigningIn} = useSignInAccount();
+  const {mutateAsync: signInAccount} = useSignInAccount();
 
-  const form = useForm<z.infer<typeof SignupValidation>>({
-    resolver: zodResolver(SignupValidation),
+  const form = useForm<z.infer<typeof SigninValidation>>({
+    resolver: zodResolver(SigninValidation),
     defaultValues: {
-      name:"",
-      username: "",
       email:"",
       password:"",
     },
   })
- async function onSubmit(values: z.infer<typeof SignupValidation>) {
-     const newUser = await createUserAccount(values)
-    if(!newUser){
-      return  toast({
-        title: "Sign up failed. Try again later",
-       
-      })
-    }
+ async function onSubmit(values: z.infer<typeof SigninValidation>) {
+   
      const session = await signInAccount({
       email: values.email,
       password: values.password,
@@ -71,39 +62,12 @@ const SigninForm = () => {
        <Form {...form}>
         <div className="sm:w-420 flex-center flex-col">
           <img src="/assets/images/logo.svg" />
-          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
-          <p className="text-light-3 small-medium md:base-regular mt-2">To use Snapgram enter your details</p>
+          <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Log in to your account</h2>
+          <p className="text-light-3 small-medium md:base-regular mt-2">Welcome back</p>
 
       
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" {...field} />
-              </FormControl>
-            
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input type="text" className="shad-input" {...field} />
-              </FormControl>
-            
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+       
           <FormField
           control={form.control}
           name="email"
@@ -133,15 +97,15 @@ const SigninForm = () => {
           )}
         />
         <Button type="submit" className="shad-button_primary">
-          {isCreatingUser ? (
+          {isUserLoading ? (
             <div className="flex-center gap-2">
              <Loader/> Loading...
             </div>
           ): "Sign up"}
         </Button>
         <p className="text-small-regular text-light-2 text-center mt-2">
-            Already have an account?
-            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+            Don't have an account?
+            <Link to="/sign-up" className="text-primary-500 text-small-semibold ml-1">Sign up</Link>
         </p>
       </form>
       </div>
--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const RootLayout = () => {
+const Profile = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const RootLayout = () => {
   )
 }
 
-export default RootLayout
+export default Profile
